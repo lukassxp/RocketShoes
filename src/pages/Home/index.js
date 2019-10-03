bdfc40cb9ee144d0403476d2cbab803b7d24c615
lukassxp <lpalucas.10@gmail.com>
2019-10-03 14:26:19 -0300

    created structure of home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-shox-nz-eu-masculino/14/D12-9970-014/D12-9970-014_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nike Shox</strong>
+        <span>R$499,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 1
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
