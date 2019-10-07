92469037c3547f677e4f1bc21ac28c6f42e9270f
lukassxp <lpalucas.10@gmail.com>
2019-10-07 00:50:26 -0300

    added page transition after add product to cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -9,18 +9,19 @@ import './config/ReactotronConfig';
 import Header from './components/Header/index';
 import Routes from './routes';
 import store from './store';
+import history from './services/history';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
